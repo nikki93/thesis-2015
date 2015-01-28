25618a2348906d62071c273601038e4cfbd36dff
nikki93 <s.nikhilesh@gmail.com>
2015-01-28 17:27:51 -0500

    better error functions

--- oculus-pcloud/oculus-pcloud/error.c
+++ oculus-pcloud/oculus-pcloud/error.c
@@ -1,11 +1,32 @@
 #include "error.h"
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void error(const char *fmt)
+static void _error(const char *s)
 {
-    fputs(fmt, stderr);
+    fputs(s, stderr);
     exit(1);
 }
 
+void errorf(const char *fmt, ...)
+{
+    va_list ap1, ap2;
+    unsigned int n;
+    char *s;
+
+    va_start(ap1, fmt);
+    va_copy(ap2, ap1);
+
+    /* how much space do we need? */
+    n = vsnprintf(NULL, 0, fmt, ap2);
+    va_end(ap2);
+
+    /* allocate, sprintf, print */
+    s = malloc(n + 1);
+    vsprintf(s, fmt, ap1);
+    va_end(ap1);
+    _error(s);
+    free(s);
+}
--- oculus-pcloud/oculus-pcloud/error.h
+++ oculus-pcloud/oculus-pcloud/error.h
@@ -1,2 +1,19 @@
-void error(const char *fmt); /* print error message and exit */
+/* printf-style error formatting */
+void errorf(const char *fmt, ...);
+
+/* evaluates to a 'filename:linenumber: ' string */
+#define line_str__(line)                        \
+    __FILE__ ":" #line ": "
+#define line_str_(line)                         \
+    line_str__(line)
+#define line_str()                              \
+    line_str_(__LINE__)
+
+/* printf-style error formatting with file name, line number */
+#define error(...)                              \
+    errorf(line_str() __VA_ARGS__)
+
+/* printf-style error formatting for an assertion, also prints condition */
+#define error_assert(cond, ...)                         \
+    ((cond) ? 0: error("assertion '" #cond "' failed ... " __VA_ARGS__))
 
