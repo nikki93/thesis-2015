d444c67b5c9a81b71779305ecfae0a748012592c
nikki93 <s.nikhilesh@gmail.com>
2015-02-01 03:51:00 -0500

    expose vec4, swizzle operators

--- ds-ovr/maths.h
+++ ds-ovr/maths.h
@@ -1,11 +1,13 @@
 #ifndef MATHS_H
 #define MATHS_H
 
+#define GLM_SWIZZLE
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 using glm::vec2;
 using glm::vec3;
+using glm::vec4;
 using glm::mat4;
 using glm::quat;
 
